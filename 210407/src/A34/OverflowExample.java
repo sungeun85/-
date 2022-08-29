4eed921fc17f05eced7714a9f6a13d0587094b86
sungeun85 <102279783+sungeun85@users.noreply.github.com>
2022-08-29 23:30:21 +0900

    Add files via upload

---
+++ 210407/src/A34/OverflowExample.java
@@ -0,0 +1,10 @@
+package A34;
+
+public class OverflowExample {
+	public static void main(String[] args) {
+		long x = 1000000;
+		long y = 1000000;
+		long z = x * y;
+		System.out.println(z);
+	}
+}
---
+++ 210407/src/CharOperatorExample.java
@@ -0,0 +1,11 @@
+
+public class CharOperatorExample {
+	public static void main(String[] agrs) {
+		char c1 = 'A' + 1;
+		char c2 = 'A';
+		int c3 = c2 + 1;
+		System.out.println("c1: " + c1);
+		System.out.println("c2: " + c2);
+		System.out.println("c3: " + c3);
+	}
+}
